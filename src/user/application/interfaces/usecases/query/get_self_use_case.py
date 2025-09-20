22a8386bf67e92e1e4b71a128d8a2ada30b54405
MDx3R <nationmdxr@gmail.com>
2025-09-20 22:31:24 +0300

    rename identity to user

---
+++ src/user/application/interfaces/usecases/query/get_self_use_case.py
@@ -0,0 +1,11 @@
+from abc import ABC, abstractmethod
+
+from user.application.dtos.queries.get_user_be_id_query import (
+    GetUserByIdQuery,
+)
+from user.application.dtos.responses.user_dto import UserDTO
+
+
+class IGetSelfUseCase(ABC):
+    @abstractmethod
+    async def execute(self, query: GetUserByIdQuery) -> UserDTO: ...
