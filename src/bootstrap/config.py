4f5e37053e94eb7a8d1297956af53e0fd8bd266e
MDx3R <nationmdxr@gmail.com>
2025-09-27 15:39:05 +0300

    fix: implement abstract masked_dict

--- src/bootstrap/config.py
+++ src/bootstrap/config.py
@@ -1,3 +1,5 @@
+from typing import Any
+
 from common.infrastructure.config.config import Settings
 from common.infrastructure.config.database_config import DatabaseConfig
 from common.infrastructure.config.logger_config import LoggerConfig
@@ -8,3 +10,9 @@ class AppConfig(Settings):
     auth: AuthConfig
     db: DatabaseConfig
     logger: LoggerConfig
+
+    def masked_dict(self) -> dict[str, Any]:
+        return self.model_dump(
+            mode="json",
+            exclude={"db": {"db_password"}, "auth": {"secret_key", "algorithm"}},
+        )
