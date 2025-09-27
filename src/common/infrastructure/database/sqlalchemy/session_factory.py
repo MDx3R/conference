5e6104f17f9c984763e15d2bc18160951bf7c805
MDx3R <nationmdxr@gmail.com>
2025-09-27 12:19:24 +0300

    feat: add database class to encapsulate database init and managment logic

--- src/common/infrastructure/database/sqlalchemy/session_factory.py
+++ src/common/infrastructure/database/sqlalchemy/session_factory.py
@@ -8,14 +8,17 @@ class ISessionFactory(ABC):
     def create(self) -> AsyncSession: ...
 
 
-class MakerSessionFactory(ISessionFactory):
-    _maker: async_sessionmaker[AsyncSession]
+MAKER = async_sessionmaker[AsyncSession]
 
-    def __init__(self, maker: async_sessionmaker[AsyncSession]) -> None:
+
+class MakerSessionFactory(ISessionFactory):
+    _maker: MAKER
+
+    def __init__(self, maker: MAKER) -> None:
         self._maker = maker
 
     def create(self) -> AsyncSession:
         return self._maker()
 
-    def set_maker(self, maker: async_sessionmaker[AsyncSession]) -> None:
+    def set_maker(self, maker: MAKER) -> None:
         self._maker = maker
