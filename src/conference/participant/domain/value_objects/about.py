3873f1b9d3f82d2e51f284037740d5be030e48cd
MDx3R <nationmdxr@gmail.com>
2025-10-01 22:46:59 +0300

    feat: add primitive domain methods to participant domain model and related VOs

--- src/conference/participant/domain/value_objects/about.py
+++ src/conference/participant/domain/value_objects/about.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from typing import Self
 
 from conference.participant.domain.value_objects.enums import (
     AcademicDegree,
@@ -14,3 +15,21 @@ class About:
     academic_title: AcademicTitle
     research_area: ResearchArea
     workplace: Workplace | None
+
+    @classmethod
+    def create(
+        cls,
+        academic_degree: AcademicDegree | None,
+        academic_title: AcademicTitle | None,
+        research_area: ResearchArea | None,
+        workplace: Workplace | None,
+    ) -> Self:
+        academic_degree = academic_degree or AcademicDegree.NONE
+        academic_title = academic_title or AcademicTitle.NONE
+        research_area = research_area or ResearchArea.NONE
+        return cls(
+            academic_degree=academic_degree,
+            academic_title=academic_title,
+            research_area=research_area,
+            workplace=workplace,
+        )
