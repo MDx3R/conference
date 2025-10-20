f9e7331a95e7e48f431031e9e7086dc021b94d65
malivartti <malivartti@yandex.ru>
2025-10-20 21:33:50 +0300

    fix: refactor app initialization to support middleware registration

--- src/common/infrastructure/app/app.py
+++ src/common/infrastructure/app/app.py
@@ -1,10 +1,12 @@
 import logging
-from abc import ABC, abstractmethod
 
+from common.infrastructure.app.http_app import IHTTPApp
+from common.infrastructure.app.iapp import IApp
 from common.infrastructure.server.fastapi.middleware.error_middleware import (
     ApplicationErrorHandler,
     DomainErrorHandler,
     ErrorHandlingMiddleware,
+    IHTTPErrorHandler,
     RepositoryErrorHandler,
 )
 from common.infrastructure.server.fastapi.middleware.logging_middleware import (
@@ -13,28 +15,32 @@ from common.infrastructure.server.fastapi.middleware.logging_middleware import (
 from common.infrastructure.server.fastapi.server import FastAPIServer
 
 
-class IApp(ABC):
-    @abstractmethod
-    def configure(self) -> None: ...
-
-
 class App(IApp):
     def __init__(self, logger: logging.Logger, server: FastAPIServer) -> None:
         self.logger = logger
         self.server = server
+        self.sub_apps: list[IHTTPApp] = []
 
     def configure(self) -> None:
-        """Must be called after configuration of sub apps."""
-        self.server.use_middleware(
-            ErrorHandlingMiddleware,
-            handlers=[
-                RepositoryErrorHandler(),  # Must be before ApplicationErrorHandler since RepositoryError is subtype of ApplicationError
+        all_handlers: list[IHTTPErrorHandler] = []
+
+        for sub_app in self.sub_apps:
+            all_handlers.extend(sub_app.error_handlers())
+
+        all_handlers.extend(
+            [
+                RepositoryErrorHandler(),
                 ApplicationErrorHandler(),
                 DomainErrorHandler(),
-            ],
+            ]
         )
-        self.server.use_middleware(LoggingMiddleware, logger=self.logger)
+
         self.server.include_cors_middleware()
+        self.server.use_middleware(LoggingMiddleware, logger=self.logger)
+        self.server.use_middleware(
+            ErrorHandlingMiddleware,
+            handlers=all_handlers,
+        )
 
     def add_app(self, *apps: IApp) -> None:
         for app in apps:
--- src/common/infrastructure/app/http_app.py
+++ src/common/infrastructure/app/http_app.py
@@ -1,12 +1,18 @@
 from abc import abstractmethod
 
-from common.infrastructure.app.app import IApp
+from common.infrastructure.app.iapp import IApp
+from common.infrastructure.server.fastapi.middleware.error_middleware import (
+    IHTTPErrorHandler,
+)
 
 
 class IHTTPApp(IApp):
     def configure(self) -> None:
         self.configure_dependencies()
         self.register_routers()
+
+    def error_handlers(self) -> list[IHTTPErrorHandler]:
+        return []
 
     @abstractmethod
     def configure_dependencies(self) -> None: ...
