e96b794d893da7e61f3c86092ec901058f08b314
MDx3R <nationmdxr@gmail.com>
2025-12-19 07:53:07 +0300

    fix: remove intended condition for test failure

--- tests/unit/idp/identity/domain/entity/test_identity.py
+++ tests/unit/idp/identity/domain/entity/test_identity.py
@@ -7,8 +7,6 @@ from idp.identity.domain.entity.identity import Identity
 
 class TestIdentity:
     def test_create(self) -> None:
-        raise Exception
-
         # Arrange
         identity_id = uuid4()
         username = "testuser"
